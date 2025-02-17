29ef7c4fb9628f7b360fdbf6d7f4adb1196f2c38
Lisa Hauschild <lisa_hauschild@hotmail.com>
2025-02-17 16:13:05 +0100

    add input and button

--- JS/js-inputs-and-strings_switch-values/js/index.js
+++ JS/js-inputs-and-strings_switch-values/js/index.js
@@ -3,6 +3,8 @@ console.clear();
 const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -11,3 +13,9 @@ uppercaseButton.addEventListener("click", () => {
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
 });
+
+switchButton.addEventListener("click",() => {
+  
+  secondInput.value = firstInput.value 
+  firstInput.value = secondInput
+})
